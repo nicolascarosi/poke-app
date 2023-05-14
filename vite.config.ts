f247be3f7349cd3b72bd05147a619f3a5e79f954
Nicolás Carosi <64372950+nicolascarosi@users.noreply.github.com>
2023-05-14 18:06:52 -0300

    gh-pages scripts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
